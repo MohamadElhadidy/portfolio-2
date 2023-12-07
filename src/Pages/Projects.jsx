5c58a609840675ed460801fe49185ae53f689b3c
MohamadElhadidy <elhadidy.384@gmail.com>
2023-12-07 15:40:04 +0200

    started in projects section

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <section className='max-container'>
+      <h1 className='head-text'>My<span className='blue-gradient_text font-semibold drop-shadow'>Projects</span></h1>
+      <div className='mt-5 flex felx-col gap-3 text-slate-500'>
+        <p>Experienced Full-Stack Web Developer with 4+ years' experience. Skilled in HTML, CSS, PHP, JavaScript, Laravel,
+        </p>
+      </div>
+
+      
+      </section>
   )
 }
 
